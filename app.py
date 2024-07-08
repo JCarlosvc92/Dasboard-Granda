353f533cf86673e55a7f76c26d31574f67771177
JCarlosvc92 <160148133+JCarlosvc92@users.noreply.github.com>
2024-07-08 14:51:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def login():
     password = st.text_input("Contraseña", type="password")
     
     if st.button("Iniciar Sesión"):
-        if username == "admin" and password == "admin":
+        if username == "Usuario1" and password == "lcm2024":
             st.session_state["logged_in"] = True
             st.experimental_rerun()
         else:
