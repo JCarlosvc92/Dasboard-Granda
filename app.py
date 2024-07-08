222d7cb5ad63eabd6ffae1634bd3a5f1f76b6c54
JCarlosvc92 <160148133+JCarlosvc92@users.noreply.github.com>
2024-07-08 10:19:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ def main():
     '''
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-    st.markdown(
-     f'<div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 80%;">'
-     f'<img src="{logo_image_path}" style="width: 200px;">'  # Ajusta el tamaño del logo aquí
-     '</div>',
-     unsafe_allow_html=True
-  )
+   st.markdown(
+    f'<div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100%;">'
+    f'<img src="{logo_image_path}" style="width: 300px;">'
+    '</div>',
+    unsafe_allow_html=True
+)
 
 
     # Título
